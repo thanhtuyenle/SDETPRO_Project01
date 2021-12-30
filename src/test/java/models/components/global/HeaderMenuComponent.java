464aafb61623b2d5f9f64b6339fb24cfd3daff94
thanhtuyenle <ttle@mum.edu>
2021-12-29 20:50:01 -0600

    task08 complete verify footer for a random menu

---
+++ src/test/java/models/components/global/HeaderMenuComponent.java
@@ -0,0 +1,29 @@
+package models.components.global;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.Locale;
+
+public class HeaderMenuComponent {
+    private final WebElement component;
+    private final WebDriver driver;
+
+    public HeaderMenuComponent(WebDriver driver) {
+        this.driver = driver;
+        this.component = driver.findElement(By.cssSelector(".header-menu"));
+    }
+    public List<WebElement> topMenuHeaderLinks() {
+        return component.findElements(By.cssSelector(".top-menu > li > a"));
+    }
+
+    public void waitUntilCategoryPageLoad(String categoryName) {
+        WebDriverWait wait = new WebDriverWait(driver, 15);
+        wait.until(ExpectedConditions.urlContains(
+                categoryName.toLowerCase().replaceAll(" ","-").replace("&", "-")));
+    }
+}
--- src/test/java/tests/pages/CategoryPage.java
+++ src/test/java/tests/pages/CategoryPage.java
@@ -1,4 +1,30 @@
 package tests.pages;
 
-public class CategoryPage {
+import models.components.global.HeaderMenuComponent;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import testdata.url.URL;
+import testflows.global.FooterFlow;
+import tests.BaseTest;
+
+import java.security.SecureRandom;
+import java.util.List;
+
+public class CategoryPage extends BaseTest {
+    @Test
+    public void TestFooterComponent() {
+        WebDriver driver = getDriver();
+        goTo(URL.HOMEPAGE);
+
+        HeaderMenuComponent headerMenuComponent = new HeaderMenuComponent(driver);
+        List<WebElement> topMenuHeaderLinks = headerMenuComponent.topMenuHeaderLinks();
+        WebElement randomHeader = topMenuHeaderLinks.get(new SecureRandom().nextInt(topMenuHeaderLinks.size()));
+        String categoryName = randomHeader.getText();
+        randomHeader.click();
+        headerMenuComponent.waitUntilCategoryPageLoad(categoryName);
+
+        FooterFlow footerFlow = new FooterFlow(driver);
+        footerFlow.verifyFooterComponent();
+    }
 }
