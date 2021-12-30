1f537f886640e4dd29242d6546a4a4f71314a568
thanhtuyenle <ttle@mum.edu>
2021-12-29 20:55:25 -0600

    task08 verify footer for shoppingcartpage

--- src/test/java/tests/pages/ShoppingCartPage.java
+++ src/test/java/tests/pages/ShoppingCartPage.java
@@ -1,4 +1,18 @@
 package tests.pages;
 
-public class ShoppingCartPage {
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+import testdata.url.URL;
+import testflows.global.FooterFlow;
+import tests.BaseTest;
+
+public class ShoppingCartPage extends BaseTest {
+    @Test
+    public void verifyFooterComponent() {
+        WebDriver driver = getDriver();
+        goTo(URL.CART);
+        FooterFlow footerFlow = new FooterFlow(driver);
+        footerFlow.verifyFooterComponent();
+    }
+
 }
